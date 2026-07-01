8d41e731544f3184d812f0265ecfa6ce931457a2
GeneralFlex <kloudavincent@gmail.com>
2026-07-02 01:05:32 +0200

    Conveyor belt system mk3
    Reworked the conveyor system again

--- Assets/_Scripts/Structures/Logistics/ConveyorBelt.cs
+++ Assets/_Scripts/Structures/Logistics/ConveyorBelt.cs
@@ -1,88 +1,95 @@
-using NUnit.Framework;
 using UnityEngine;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEngine.Windows;
-using System.Collections;
 using System.Linq;
+using System.Collections;
+using Gaskellgames;
+using UnityEditor.VersionControl;
+using UnityEditor;
+using NUnit.Framework;
 
+[System.Serializable]
+public class ConveyorBeltShape
+{
+    public string name;
+    public Texture2D spriteSheet;
+    public List<Sprite> sprites;
+    public List<Vector2> inputs;
+    public void SliceSpriteSheet()
+    {
+        string path = AssetDatabase.GetAssetPath(spriteSheet); 
+        Object[] assets = AssetDatabase.LoadAllAssetsAtPath(path); 
+        sprites = assets.OfType<Sprite>().OrderBy(s => s.name).ToList();
+    }
+}
 
 public class ConveyorBelt : MonoBehaviour
 {
-    [Header("input/output settings")]
-    public int groupID = 1;
-    public Vector2 direction;
-    public ConveyorBeltDirection conveyorBeltDirection;
+    [Header("Conveyor settings")]
+    public int groupIndex;
+    private ConveyorBeltGroup group => ConveyorBeltManager.Instance.groups[groupIndex];
+    public Vector2Int direction;
     public ConveyorBelt output;
-    public List<ConveyorBelt> inputs = new List<ConveyorBelt>();
-
-    private ConveyorPort conveyorPort = new ConveyorPort();
+    public List<ConveyorBelt> inputs;
+    public List<ConveyorBeltShape> shapes;
+    private ConveyorBeltShape currentShape;
 
     [Header("References")]
-    public SpriteRenderer spriteRenderer;
-    private ConveyorBeltGroup group;
-
-    private IEnumerator Start()
+    public SpriteRenderer conveyorSpriteRenderer;
+    
+    private void Start()
     {
-        yield return null;
-
-        group = ConveyorBeltManager.Instance.GetGroup(groupID);
-
-        //setup conveyor port
         HashSet<Vector2> dirs = new HashSet<Vector2>();
         dirs.Add(Vector2.up);
         dirs.Add(Vector2.right);
         dirs.Add(Vector2.down);
         dirs.Add(Vector2.left);
 
-        conveyorPort.portTypes.Add(direction, ConveyorPortType.Output);
-
-        foreach (var dir in dirs)
+        foreach (Vector2 dir in dirs)
         {
             if (TileManager.Instance.GetTileObject((Vector3)dir + transform.position, transform.GetComponent<TileObject>().SO.displayName) is TileObject tileObject)
             {
-                ConveyorBelt neighbourConveyor = tileObject.GetComponent<ConveyorBelt>();
-                if (neighbourConveyor.direction + dir == Vector2.zero)
+                ConveyorBelt belt = tileObject.GetComponent<ConveyorBelt>();
+                if (belt.direction + dir == Vector2.zero)
                 {
-                    conveyorPort.portTypes.Add(dir, ConveyorPortType.Input);
-                    neighbourConveyor.Refresh();
-                    inputs.Add(neighbourConveyor);
+                    belt.output = this;
+                    inputs.Add(belt);
+                    belt.Refresh();
                 }
-                if (dir == direction)
-                { 
-                    neighbourConveyor.conveyorPort.portTypes.Add(dir*-1, ConveyorPortType.Input);
-                    neighbourConveyor.inputs.Add(this);
-                    output = neighbourConveyor;
-                    neighbourConveyor.Refresh();
+                else if (direction == dir)
+                {
+                    output = belt;
+                    belt.inputs.Add(this);
+                    belt.Refresh();
                 }
             }
         }
-
         Refresh();
-
-        group.updateFrame.AddListener(UpdateFrame);
+        group.newFrame.AddListener(Animate);
     }
-
-    private void UpdateFrame(int frame)
+    [ContextMenu("Slice sprite sheets")]
+    private void SliceSpriteSheets()
     {
-        spriteRenderer.sprite = conveyorBeltDirection.sprites[frame];
-    }
-
-    private void OnDestroy()
-    {
-        if (!Application.isPlaying)
-                return;
-
-        if (output != null)
-        {
-            output.inputs.Remove(this);
-            output.conveyorPort.portTypes.Remove(direction);
-            output.Refresh();
-        }
+        foreach (ConveyorBeltShape shape in shapes)
+            shape.SliceSpriteSheet();
     }
 
     public void Refresh()
     {
-        conveyorBeltDirection = ConveyorBeltManager.Instance.GetDirection(group.ID, conveyorPort);
+        List<Vector2> dirs = new List<Vector2>();
+
+        foreach (Vector2 dir in inputs.Select(r => r.direction))
+            if(dir!=-direction)
+                dirs.Add(dir);
+
+        if (shapes.FirstOrDefault(r => new HashSet<Vector2>(r.inputs).SetEquals(dirs)) is ConveyorBeltShape shape)
+            currentShape = shape;
+        else currentShape = shapes.FirstOrDefault();
+    }
+
+    public void Animate(int frame) 
+    {
+        if(conveyorSpriteRenderer == null || currentShape == null)
+            return;
+        conveyorSpriteRenderer.sprite = currentShape.sprites[frame];
     }
 }
--- Assets/_Scripts/Structures/Logistics/ConveyorBeltManager.cs
+++ Assets/_Scripts/Structures/Logistics/ConveyorBeltManager.cs
@@ -1,211 +1,48 @@
-using UnityEngine;
-using System.Collections.Generic;
 using System.Collections;
-using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Events;
-using UnityEditor;
-using Unity.VisualScripting;
-
-
-[System.Serializable]
-public enum ConveyorPortType
-{
-    None,
-    Input,
-    Output
-}
-
-[System.Serializable]
-public class ConveyorPort
-{
-    public ConveyorPortType up;
-    public ConveyorPortType right;
-    public ConveyorPortType down;
-    public ConveyorPortType left;
-
-    public Dictionary<Vector2, ConveyorPortType> portTypes = new Dictionary<Vector2, ConveyorPortType>();
-
-    public bool HasEqualPorts(ConveyorPort port)
-    {
-        foreach(KeyValuePair<Vector2, ConveyorPortType> kvp in portTypes)
-        {
-            if(!port.portTypes.ContainsKey(kvp.Key) && portTypes[kvp.Key]==ConveyorPortType.None)
-                continue;
-            if (!port.portTypes.ContainsKey(kvp.Key) || port.portTypes[kvp.Key] != kvp.Value)
-                return false;
-        }
-        return true;
-    }
-    public void RebuildDictionary()
-    {
-        portTypes.Clear();
-
-        portTypes[Vector2.up] = up;
-        portTypes[Vector2.right] = right;
-        portTypes[Vector2.down] = down;
-        portTypes[Vector2.left] = left;
-    }
-}
-
-[System.Serializable]
-public class ConveyorBeltDirection
-{
-    public string displayName;
-    public Vector2 direction;
-    public List<ConveyorPort> ports;
-    public Texture2D sheet;
-    //[HideInInspector]
-    public List<Sprite> sprites;
-}
 
 [System.Serializable]
 public class ConveyorBeltGroup
 {
-    [Header("Config")]
-    public int ID;
-    public List<ConveyorBeltDirection> directions;
+    public int speed;
+    [HideInInspector]public UnityEvent<int> newFrame = new UnityEvent<int>();
+    private int animationIndex;
 
-    [Header("Animation")]
-    public float speed;
-    public int animationFrame = 0;
-    [HideInInspector]
-    public UnityEvent<int> updateFrame;
+    public IEnumerator Animate()
+    {
+        while (true)
+        {
+            animationIndex = (animationIndex + 1) % 4;
+            newFrame.Invoke(animationIndex);
+            yield return new WaitForSeconds(1f / speed);
+        }
+    }
 }
 
 public class ConveyorBeltManager : MonoBehaviour
 {
-    public List<ConveyorBeltGroup> conveyorGroups = new List<ConveyorBeltGroup>();
-    public int index=0;
-
     public static ConveyorBeltManager Instance { get; private set; }
+    public List<ConveyorBeltGroup> groups;
 
     private void Awake()
     {
-        Instance = this;
-        foreach(ConveyorBeltGroup cg in conveyorGroups)
+        if (Instance != null && Instance != this)
         {
-            foreach(ConveyorBeltDirection direction in cg.directions)
-            {
-                foreach (ConveyorPort port in direction.ports)
-                {
-                    port.RebuildDictionary();
-                }
-            }
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
         }
     }
 
     private void Start()
     {
-        foreach (ConveyorBeltGroup cg in conveyorGroups)
+        foreach (ConveyorBeltGroup group in groups)
         {
-            StartCoroutine(Animate(cg));
+            StartCoroutine(group.Animate());
         }
-    }
-
-    public ConveyorBeltDirection GetDirection(int groupID, ConveyorPort port)
-    {
-        ConveyorBeltGroup group = conveyorGroups.Where(r => r.ID == groupID).ToList().First();
-        return group.directions.Where(r => r.ports.Any(l => l.HasEqualPorts(port)==true)).ToList().First();
-    }
-
-    [ContextMenu("debug")]
-    public void debug()
-    {
-        HashSet<Vector2> dirs = new HashSet<Vector2>();
-        dirs.Add(Vector2.up);
-        dirs.Add(Vector2.right);
-        dirs.Add(Vector2.down);
-        dirs.Add(Vector2.left);
-        foreach (var group in conveyorGroups)
-        {
-            Dictionary<ConveyorPort, ConveyorBeltDirection> ports = new Dictionary<ConveyorPort, ConveyorBeltDirection>();
-
-            foreach (ConveyorBeltDirection direction in group.directions)
-            {
-                foreach (ConveyorPort port in direction.ports)
-                {
-                    foreach(Vector2 dir in dirs)
-                    {
-                        try
-                        {
-                            Debug.Log(port.portTypes[dir]);
-                        }
-                        catch
-                        {
-                            Debug.Log(direction.displayName);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    [ContextMenu("Check For Duplicates")]
-    public void CheckForDuplicates()
-    {
-        foreach (var group in conveyorGroups)
-        {
-            Dictionary<ConveyorPort, ConveyorBeltDirection> ports = new Dictionary<ConveyorPort, ConveyorBeltDirection>();
-
-            foreach (ConveyorBeltDirection direction in group.directions)
-            {
-                foreach (ConveyorPort port in direction.ports)
-                {
-                    foreach (ConveyorPort port2 in ports.Keys) {
-                        if(port2.HasEqualPorts(port))
-                            Debug.LogWarning("Found duplicate:      " + direction.displayName + " [" + direction.ports.IndexOf(port) + "]    x    " + ports[port2].displayName + " [" + ports[port2].ports.IndexOf(port2) + "]");
-                    }
-                    try
-                    {
-                        ports.Add(port, direction);
-                    }
-                    catch { }
-                }
-            }
-        }
-        Debug.Log("Check done");
-    }
-
-    [ContextMenu("Slice Sprite Sheet")]
-    private void SliceSpriteSheet()
-    {
-        foreach (ConveyorBeltGroup group in conveyorGroups)
-        {
-            foreach (ConveyorBeltDirection direction in group.directions)
-            {
-                if (direction == null || direction.sheet == null)
-                    continue;
-
-                string path = AssetDatabase.GetAssetPath(direction.sheet);
-
-                Object[] assets = AssetDatabase.LoadAllAssetsAtPath(path);
-
-                direction.sprites ??= new List<Sprite>();
-                direction.sprites.Clear();
-
-                foreach (Object asset in assets)
-                    if (asset is Sprite sprite)
-                        direction.sprites.Add(sprite);
-
-                direction.sprites = direction.sprites.OrderBy(s => s.name).ToList();
-            }
-        }
-    }
-
-    public ConveyorBeltGroup GetGroup(int ID)
-    {
-        return conveyorGroups.Where(r => r.ID == ID).ToList().First();
-    }
-
-    private IEnumerator Animate(ConveyorBeltGroup cg)
-    {
-        cg.animationFrame = (cg.animationFrame+1)%4;
-        if (cg.animationFrame > 4)
-            cg.animationFrame = 0;
-
-        cg.updateFrame?.Invoke(cg.animationFrame);
-
-        yield return new WaitForSeconds(1/cg.speed);
-        StartCoroutine(Animate(cg));
     }
 }
